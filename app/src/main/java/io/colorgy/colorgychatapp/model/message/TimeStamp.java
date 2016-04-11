386ed0105abcbea4798320cfc67c1e6e9a64dd5c
Jean Yang <bdlas321@gmail.com>
2016-04-11 17:59:41 +0800

    update TimeStamp Model

--- app/src/main/java/io/colorgy/colorgychatapp/model/message/TimeStamp.java
+++ app/src/main/java/io/colorgy/colorgychatapp/model/message/TimeStamp.java
@@ -1,10 +1,32 @@
 package io.colorgy.colorgychatapp.model.message;
 
+import java.util.Calendar;
+
 /**
- * Created by apple on 2016/4/11.
+ * Created by erwaiyang on 2016/4/11.
  */
 public class TimeStamp {
-    public TimeStamp(String createdAt){
 
+    private int year;
+    private int month;
+    private int date;
+    private int hour;
+    private int minute;
+    private int second;
+    private int millisecond;
+
+    public TimeStamp(String timeStampString){
+
+    }
+
+    public TimeStamp(){
+        Calendar c = Calendar.getInstance();
+        this.year = c.get(Calendar.YEAR);
+        this.month = c.get(Calendar.MONTH);
+        this.date = c.get(Calendar.DATE);
+        this.hour = c.get(Calendar.HOUR);
+        this.minute = c.get(Calendar.MINUTE);
+        this.second = c.get(Calendar.SECOND);
+        this.millisecond = c.get(Calendar.MILLISECOND);
     }
 }
