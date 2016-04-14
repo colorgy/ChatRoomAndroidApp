d53abe173995a94857fb54d206bd9087726b8048
Jean Yang <bdlas321@gmail.com>
2016-04-14 17:49:30 +0800

    resolve R

--- app/src/main/java/io/colorgy/colorgychatapp/view/UserDetailInformationActivity.java
+++ app/src/main/java/io/colorgy/colorgychatapp/view/UserDetailInformationActivity.java
@@ -3,6 +3,8 @@ package io.colorgy.colorgychatapp.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.colorgy.colorgychatapp.R;
+
 public class UserDetailInformationActivity extends AppCompatActivity {
 
     @Override
